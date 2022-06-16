3c6085ae27d62e531ae6ed4688af06cfcbdf8b18
Gerrudo <tom@tomsnetwork.uk>
2022-06-16 19:11:54 +0100

    Completed Bob

--- exercises/practice/bob/BobResponse.ps1
+++ exercises/practice/bob/BobResponse.ps1
@@ -6,16 +6,16 @@ Function Get-BobResponse() {
     .DESCRIPTION
     Bob is a lackadaisical teenager. In conversation, his responses are very limited.
 
-    Bob answers 'Sure.' if you ask him a question.
+    Bob answers "Sure." if you ask him a question.
 
-    He answers 'Whoa, chill out!' if you yell at him.
+    He answers "Whoa, chill out!" if you yell at him.
 
-    He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
+    He answers "Calm down, I know what I"m doing!" if you yell a question at him.
 
-    He says 'Fine. Be that way!' if you address him without actually saying
+    He says "Fine. Be that way!" if you address him without actually saying
     anything.
 
-    He answers 'Whatever.' to anything else.
+    He answers "Whatever." to anything else.
     
     .PARAMETER HeyBob
     The sentence you say to Bob.
@@ -25,8 +25,24 @@ Function Get-BobResponse() {
     #>
     [CmdletBinding()]
     Param(
-        [string]$HeyBob
+        [string]$heyBob
     )
+    $responses = [PSCustomObject]@{
+        question     = [string]"Sure.";
+        yell         = [string]"Whoa, chill out!";
+        yellQuestion = [string]"Calm down, I know what I'm doing!";
+        nothing      = [string]"Fine. Be that way!";
+        default      = [string]"Whatever.";
+    }
+    [boolean]$yelling = $false
+    [boolean]$question = $false
+    $heyBob = $heyBob.trim()
+    if (($heyBob.toUpper() -ceq $heyBob) -and ($heyBob -cmatch "[A-Za-z]")) { $yelling = $true }
+    if ($heyBob[$heyBob.length - 1] -eq "?") { $question = $true }
 
-    Throw "Function not implemented"
+    if ([string]::IsNullOrEmpty($heyBob)) {return $responses.nothing}
+    if ($question -and $yelling ) { return $responses.yellQuestion }
+    if ($question) { return $responses.question }
+    if ($yelling) { return $responses.yell }
+    return $responses.default
 }
