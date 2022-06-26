d1b2c4b7e3070c05b7fe4b1a8d7f90f81cd6acf9
Gerrudo <tom@tomsnetwork.uk>
2022-06-26 22:57:37 +0100

    Moved Collatz Conjecture, Updated Sum of Multiples

--- exercises/practice/medium/collatz-conjecture/CollatzConjecture.ps1
+++ exercises/practice/medium/collatz-conjecture/CollatzConjecture.ps1
@@ -15,7 +15,18 @@ Function Invoke-CollatzConjecture() {
     [CmdletBinding()]
     Param(
         [Int64]$Number
+        
     )
-
-    Throw "Exercise not implemented"
+    if ($Number -le 0) { throw "error: Only positive numbers are allowed" }
+    [int]$steps = 0
+    while ($Number -ne 1) {
+        if ($Number % 2 -eq 0) { 
+            $Number =/ 2
+        }
+        else {
+            $Number = ($Number * 3) + 1 
+        }
+        $steps++
+    }
+    return $steps
 }
